501b16f802aeb9c348787d7fb915617eb17aaa73
PhilipDeegan <philip.deegan@gmail.com>
2024-07-20 21:57:15 +0200

    werror (#5)

--- mod.cpp
+++ mod.cpp
@@ -28,15 +28,16 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#include "maiken/module/init.hpp"
 #include <cstdint>
 #include <unordered_set>
+
+#include "maiken/module/init.hpp"
 
 namespace mkn {
 namespace python3 {
 
 class ModuleMaker : public maiken::Module {
-private:
+ private:
 #if defined(_WIN32)
   const bool config_expected = 0;
 #else
@@ -49,24 +50,21 @@ private:
   mkn::kul::Dir bin;
   std::shared_ptr<kul::cli::EnvVar> path_var;
 
-protected:
+ protected:
   static void VALIDATE_NODE(YAML::Node const &node) {
     using namespace mkn::kul::yaml;
     Validator({NodeValidator("args")}).validate(node);
   }
 
-public:
-  void init(maiken::Application &a, YAML::Node const &node)
+ public:
+  void init(maiken::Application &a, YAML::Node const & /*node*/)
       KTHROW(std::exception) override {
     bool finally = 0;
-    if (!kul::env::WHICH(PY.c_str()))
-      PY = "python";
+    if (!kul::env::WHICH(PY.c_str())) PY = "python";
     PYTHON = mkn::kul::env::GET("PYTHON");
-    if (!PYTHON.empty())
-      PY = PYTHON;
+    if (!PYTHON.empty()) PY = PYTHON;
 #if defined(_WIN32)
-    if (PY.rfind(".exe") == std::string::npos)
-      PY += ".exe";
+    if (PY.rfind(".exe") == std::string::npos) PY += ".exe";
 #endif
     mkn::kul::Process p(PY);
     mkn::kul::ProcessCapture pc(p);
@@ -74,12 +72,10 @@ public:
     if (!HOME.empty()) {
 #if defined(_WIN32)
       bin = mkn::kul::Dir(HOME);
-      if (!bin)
-        KEXCEPT(kul::Exception, "$PYTHON3_HOME does not exist");
+      if (!bin) KEXCEPT(kul::Exception, "$PYTHON3_HOME does not exist");
 #else
       bin = mkn::kul::Dir("bin", HOME);
-      if (!bin)
-        KEXCEPT(kul::Exception, "$PYTHON3_HOME/bin does not exist");
+      if (!bin) KEXCEPT(kul::Exception, "$PYTHON3_HOME/bin does not exist");
 #endif
       path_var = std::make_shared<kul::cli::EnvVar>(
           "PATH", bin.real(), mkn::kul::cli::EnvVarMode::PREP);
@@ -87,7 +83,7 @@ public:
       p.var(path_var->name(), path_var->toString());
     };
 #if defined(_WIN32)
-    pyconfig_found = false; // doesn't exist on windows (generally)
+    pyconfig_found = false;  // doesn't exist on windows (generally)
 #else
     pyconfig_found = mkn::kul::env::WHICH(PY3_CONFIG.c_str());
 #endif
@@ -111,18 +107,16 @@ public:
     } catch (...) {
       KERR << "UNKNOWN ERROR CAUGHT";
     }
-    if (finally)
-      exit(2);
+    if (finally) exit(2);
     using namespace mkn::kul::cli;
-    auto &evs = a.envVars();
+
     std::string extension;
     if (pyconfig_found) {
       mkn::kul::os::PushDir pushd(a.project().dir());
       mkn::kul::Process p(PY3_CONFIG);
       mkn::kul::ProcessCapture pc(p);
       p << "--extension-suffix";
-      if (path_var)
-        p.var(path_var->name(), path_var->toString());
+      if (path_var) p.var(path_var->name(), path_var->toString());
       p.start();
       extension = pc.outs();
     } else {
@@ -134,13 +128,13 @@ public:
       p.start();
       extension = pc.outs();
     }
-    a.m_cInfo.lib_ext = mkn::kul::String::LINES(extension)[0]; // drop EOL
+    a.m_cInfo.lib_ext = mkn::kul::String::LINES(extension)[0];  // drop EOL
     a.m_cInfo.lib_prefix = "";
     a.mode(maiken::compiler::Mode::SHAR);
   }
 };
-} // namespace python3
-} // namespace mkn
+}  // namespace python3
+}  // namespace mkn
 
 extern "C" KUL_PUBLISH maiken::Module *maiken_module_construct() {
   return new mkn::python3::ModuleMaker;
